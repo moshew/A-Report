cbb80ce848e7d4408aafebc594f7f6453656f8ae
moshew <moshe.waisman@gmail.com>
2016-05-13 09:51:20 +0300

    update

--- www/app/app.js
+++ www/app/app.js
@@ -176,35 +176,25 @@ app.controller('mainController', function ($scope, $http, dataShare) {
 });
 
 app.controller('main1Controller', function ($scope, $http, dataShare) {
+    function init() {
 
-    // display the address information for all contacts
+        navigator.contacts.find(
+            [navigator.contacts.fieldType.displayName],
+            gotContacts,
+            errorHandler);
 
-    function onSuccess(contacts) {
-        for (var i = 0; i < contacts.length; i++) {
-            for (var j = 0; j < contacts[i].addresses.length; j++) {
-                alert("Pref: "         + contacts[i].addresses[j].pref          + "\n" +
-                    "Type: "           + contacts[i].addresses[j].type          + "\n" +
-                    "Formatted: "      + contacts[i].addresses[j].formatted     + "\n" +
-                    "Street Address: " + contacts[i].addresses[j].streetAddress + "\n" +
-                    "Locality: "       + contacts[i].addresses[j].locality      + "\n" +
-                    "Region: "         + contacts[i].addresses[j].region        + "\n" +
-                    "Postal Code: "    + contacts[i].addresses[j].postalCode    + "\n" +
-                    "Country: "        + contacts[i].addresses[j].country);
-                    $scope.test1 += contacts[i].addresses[j].formatted;
-            }
+    }
+
+    function errorHandler(e) {
+        console.log("errorHandler: "+e);
+    }
+
+    function gotContacts(c) {
+        console.log("gotContacts, number of results "+c.length);
+        for(var i=0, len=c.length; i<len; i++) {
+            test1 += c[i];
         }
-    };
-
-    function onError(contactError) {
-        alert('onError!');
-    };
-
-    // find all contacts
-    var options = new ContactFindOptions();
-    options.filter = "";
-    var filter = ["displayName", "addresses"];
-    navigator.contacts.find(filter, onSuccess, onError, options);
-
+    }
 });
 
 app.controller('loginController', function ($scope, $http, $mdDialog, dataShare) {
