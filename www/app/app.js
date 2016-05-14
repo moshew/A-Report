1f1182cbb9acc39280ae872578344849e90563be
moshew <moshe.waisman@gmail.com>
2016-05-14 15:00:09 +0300

    update

--- www/app/app.js
+++ www/app/app.js
@@ -191,10 +191,9 @@ app.controller('mainController', function ($scope, $rootScope, $http, $window, $
                 for (j = 0; j < contacts[i].phoneNumbers.length; j++) {
 
                     var phone = contacts[i].phoneNumbers[j].value;
-                    phone = phone.replace(/\+972\ /g, '0');
                     phone = phone.replace(/\+972/g, '0');
+                    phone = phone.replace(/\s/g, '');
                     phone = phone.replace(/\(|\)|-/g, '');
-                    phone = phone.split(' ').join('');
                     test2.push({phone: phone, name: contacts[i].name.formatted});
                 }
             } catch (err) {
