5dafcd580f418a7d7481739a0acf53f6f7364853
moshew <moshe.waisman@gmail.com>
2016-07-09 22:34:53 +0300

    update

--- www/app/app.js
+++ www/app/app.js
@@ -1,6 +1,6 @@
 ﻿// script.js
-var domain = 'http://mx.isra-net.co.il/~moridimt/';
-//var domain = 'http://a-report.co.il/';
+//var domain = 'http://mx.isra-net.co.il/~moridimt/';
+var domain = 'http://a-report.co.il/';
 /*
 document.addEventListener('deviceready', function() {
     angular.bootstrap(document, ['app']);
